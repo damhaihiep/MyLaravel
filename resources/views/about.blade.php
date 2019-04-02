dc01bd97ef3cf00b9fe35657f92ab814bc4be60d
Dam Hai Hiep <hiep.damhai@vietis.vn>
2019-04-02 09:43:12 +0700

    add profile page

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -5,6 +5,7 @@
     <div class="content">
         <div class="title">About Page</div>
         <div class="quote">Our about page!</div>
+        <h3>add new line</h3>
     </div>
 </div>
 @endsection
